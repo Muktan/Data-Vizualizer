3fa557039498f428380b4228768aa0ea298498e0
Muktan <muktan123@gmail.com>
2019-11-07 20:18:17 +0530

    simple scatter plot done
    took input  in comma sepratted form and plotted a scatter plot.

---
+++ Data_Vizualizer/Data_Vizualizer/Models/DataPoint.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace Data_Vizualizer.Models
+{
+    [DataContract]
+    public class DataPoint
+    {
+        public DataPoint(double x, double y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+
+        //Explicitly setting the name to be used while serializing to JSON.
+        [DataMember(Name = "x")]
+        public Nullable<double> X = null;
+
+        //Explicitly setting the name to be used while serializing to JSON.
+        [DataMember(Name = "y")]
+        public Nullable<double> Y = null;
+    }
+}
