242a13ceaea4b5777a8e7b47a211bef07b4b94b4
Vishnuhp01 <47145350+Vishnuhp01@users.noreply.github.com>
2019-11-09 09:30:14 +0530

    DataTypeDeciderController File

---
+++ Data_Vizualizer/Data_Vizualizer/Controllers/DataTypeDeciderController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Data_Vizualizer.Controllers
+{
+    public class DataTypeDeciderController : Controller
+    {
+        // GET: DataTypeDecider
+        public ActionResult DataTypeDecider()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult DataTypeDecider(string data)
+        {
+
+            
+            ViewBag.data = data;
+            
+            return View();
+        }
+    }
+}
